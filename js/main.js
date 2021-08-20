5d5e589a1e14634a38c16471357b2ad728424aca
hallucat <89076487+hallucat@users.noreply.github.com>
2021-08-21 01:47:57 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  var currentFloor = 2; // переменная, где хранится текущий этаж
+  var floorPath = $(".home-image path"); // каждый отдельный этаж в SVG
+  var counterUp = $(".counter-up"); /* кнопка увеличения этажа */  
+  var counterDown = $(".counter-down"); /* кнопка уменьшения этажа */
+  
+  // функция при наведении мышью на этаж
+  floorPath.on("mouseover", function () {
+    floorPath.removeClass("current-floor"); // удаляем активный класс у этажей
+    currentFloor = $(this).attr("data-floor"); // получаем значение текущего этажа
+    $(".counter").text(currentFloor); // записываем значение этажа в счётчик справа    
+  }); 
+
+  counterUp.on("click", function () { // отслеживаем клик по кнопке вверх
+     if (currentFloor < 18) { // проверяем значение этажа, оно не должно быть больше 18
+      currentFloor++; // прибавляем один этаж
+      usCurrentFloor = currentFloor.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false }); // форматируем переменную с этажом, чтобы было 02, а не 2
+      $(".counter").text(usCurrentFloor); // записываем значение этажа в счётчик справа
+      floorPath.removeClass("current-floor"); // удаляем активный класс у этажей
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // подсвечиваем текущий этаж
+    }    
+  });
+
+  counterDown.on("click", function () { // отслеживаем клик по кнопке вниз
+    if(currentFloor > 2) { // проверяем значение этажа, оно не должно быть меньше 2
+      currentFloor--; // убавляем один этаж
+      usCurrentFloor = currentFloor.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false }); // форматируем переменную с этажом, чтобы было 02, а не 2
+      $(".counter").text(usCurrentFloor); // записываем значение этажа в счётчик справа
+      floorPath.removeClass("current-floor"); // удаляем активный класс у этажей
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // подсвечиваем текущий этаж
+    }  
+  })
+});
